1cdbbee8f93cfb057a5c3ddeaf36db1156e01cf4
Alex3925 <120993369+Alex3925@users.noreply.github.com>
2024-07-19 17:24:46 +0800

    Update ngl.js

--- script/ngl.js
+++ script/ngl.js
@@ -1,68 +1,52 @@
+const axios = require('axios');
+
 module.exports = {
   config: {
-    name: "ngl",
-    version: "1.0.0",
-    role: 0,
-    credits: "Deku",
-    description: "Spam NGL",
-    usages: "[username => amount => message]",
-    cooldown: 0,
+    name: "nglSpammer",
+    version: "1.0",
+    description: "Ngl Spammer",
+    role: "user",
+    credits: "MrSenpai02",
+    cooldown: 8,
+    category: "spam"
   },
 
-  run: async ({ api, event, args }) => {
-    const axios = require("axios");
-    try {
-      const content = args
-       .join(" ")
-       .split("=>")
-       .map((item) => (item = item.trim()));
-      let username = content[0];
-      let amount = parseInt(content[1]);
-      let message = content[2];
-      let msg =
-        "Wrong format or something is missing." +
-        "\nHow to use: " +
-        this.config.usages;
-      if (!username ||!amount ||!message) return api.sendMessage(msg, event.threadID, event.messageID);
-      // if isNaN
-      if (isNaN(amount)) return api.sendMessage("Please enter a valid number.", event.threadID, event.messageID);
-      if (amount > 40) return api.sendMessage("The maximum number of request is 40.", event.threadID, event.messageID);
-      if (amount < 1) return api.sendMessage("Please enter a number greater than 0.", event.threadID, event.messageID);
-      api.setMessageReaction("⏳", event.messageID, (err) => {}, true);
-      api.sendMessage(
-        "You have successfully sent " +
-          amount +
-          " messages to " +
-          username +
-          "\nThe request is now processing...",
-        event.threadID,
-        event.messageID
-      );
-      const headers = {
-        referer: `https://ngl.link/${username}`,
-        "accept-language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
-      };
-
-      const data = {
-        username: username,
-        question: message,
-        deviceId: "ea356443-ab18-4a49-b590-bd8f96b994ee",
-        gameSlug: "",
-        referrer: "",
-      };
-
-      for (let i = 0; i < amount; i++) {
-        try {
-          await axios.post("https://ngl.link/api/submit", data, {
-            headers,
-          });
-          console.log(`Sent`);
-        } catch (e) {
-          console.log("Not sent");
-        }
-      }
-    } catch (w) {
-      return api.sendMessage(w["message"], event.threadID, event.messageID);
+  async run({ api, event, args }) {
+    if (args.length !== 2) {
+      api.sendMessage("Please provide username & number of message.", event.threadID);
+      return;
     }
-  },
+    
+    const username = args[0];
+    const count = args[1];
+    
+    const sendingSpam = `Sending "${count}" messages. Please wait...`;
+    api.sendMessage(sendingSpam, event.threadID);
+
+    const apiUrl = `http://158.101.198.227:8266/?username=${encodeURIComponent(username)}&count=${encodeURIComponent(count)}`;
+
+    axios.get(apiUrl)
+      .then(response => {
+        const data = response.data.result;
+        const { sentCount, notSendCount } = data;
+
+        let message;
+        if (sentCount === count) {
+          message = `Successfully sent all ${count} messages.`;
+        } else if (sentCount > 0) {
+          message = `Successfully sent ${sentCount} messages. Failed to send ${notSendCount} messages.`;
+        } else {
+          message = 'Failed to send any messages.';
+        }
+
+        // Add a delay before sending the actual response message
+        setTimeout(() => {
+          api.sendMessage(message, event.threadID);
+        }, 1000);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        api.sendMessage("Sorry, an error occurred while processing your request.", event.threadID);
+      });
+  }
 };
