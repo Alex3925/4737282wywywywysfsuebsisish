7e255c6da0ca06c1487e07c61abfb09472b65ecc
Alex3925 <120993369+Alex3925@users.noreply.github.com>
2024-07-19 12:48:26 +0800

    Update boxai.js

--- script/boxai.js
+++ script/boxai.js
@@ -1,28 +1,33 @@
 module.exports = {
   config: {
     name: "boxai",
+    version: "1.0",
     description: "Talk to Blackbox AI",
-    prefix: false,
-    accessableby: 0,
     category: "Utilities",
+    role: 0,
     author: "Deku",
+    usage: 'boxai [prompt]',
   },
-  start: async function({ api, event, text, react, reply, getLang }) {
+
+  async run({ api, event, args }) {
     const axios = require("axios");
     try {
-      let ask = text.join(" ");
-      if (!ask) return reply("Missing prompt!");
-      react("⏳");
-      const heru = await api.sendMessage('Searching your question please wait...', event.threadID);
-
-      const rest = (
-        await axios.get("https://joshweb.click/api/blackboxai?q=" + encodeURIComponent(ask) + "&uid=" + event.senderID)
-      ).data;
-
-      react("✅");
-      await api.editMessage('🤖 | 𝐁𝐥𝐚𝐜𝐤𝐛𝐨𝐱 𝐀𝐈 𝐑𝐞𝐬𝐩𝐨𝐧𝐬𝐞\n━━━━━━━━━━━━━━━━━━\n' + rest.result + '\n━━━━━━━━━━━━━━━━━━\nType "blackbox clear" if you want to clear conversation with blackbox', heru.messageID);
+      let ask = args.join(" ");
+      if (!ask) return api.sendMessage("Missing prompt!", event.threadID, event.messageID);
+      api.sendMessage("Searching your question please wait...", event.threadID, (err, info) => {
+        if (err) return api.sendMessage(err.message, event.threadID, event.messageID);
+        const heru = info.messageID;
+        axios.get("https://joshweb.click/api/blackboxai?q=" + encodeURIComponent(ask) + "&uid=" + event.senderID)
+          .then(response => {
+            const rest = response.data;
+            api.editMessage('🤖 | 𝐁𝐥𝐚𝐜𝐤𝐛𝐨𝐱 𝐀𝐈 𝐑𝐞𝐬𝐩𝐨𝐧𝐬𝐞\n━━━━━━━━━━━━━━━━━━\n' + rest.result + '\n━━━━━━━━━━━━━━━━━━\nType "blackbox clear" if you want to clear conversation with blackbox', heru);
+          })
+          .catch(error => {
+            api.sendMessage(error.message, event.threadID, event.messageID);
+          });
+      });
     } catch (e) {
-      return reply(e.message);
+      return api.sendMessage(e.message, event.threadID, event.messageID);
     }
   }
 };
