9c47a4d68bca1046d30dbd73a256ccca0bacce07
Alex3925 <120993369+Alex3925@users.noreply.github.com>
2024-07-19 12:51:28 +0800

    Update shoticron.js

--- script/shoticron.js
+++ script/shoticron.js
@@ -1,34 +1,72 @@
-module.exports.config = {
-  name: "shoticron",
-  version: "1.0.0",
-  role: 0,
-  credits: "Alex",
-  description: "Generate a random tiktok video per 1 hour.",
-  usages: "[]",
-  cooldown: 0,
-  hasPrefix: false,
-};
+module.exports = {
+  config: {
+    name: "shoticron",
+    version: "1.0.0",
+    role: 0,
+    credits: "Alex",
+    description: "Generate a random tiktok video per 1 hour.",
+    usages: "",
+    cooldown: 0,
+  },
 
-let autoExecute = true; // flag to turn on/off auto execute
+  autoExecute: true, // flag to turn on/off auto execute
 
-module.exports.run = async ({ api, event, args }) => {
-  // ...
-};
+  run: async ({ api, event, args }) => {
+    api.setMessageReaction("⏳", event.messageID, (err) => {}, true);
+    api.sendTypingIndicator(event.threadID, true);
 
-setInterval(async () => {
-  if (autoExecute) {
-    api.sendMessage("AUTO SHOTI SENT 🥵", event.threadID, event.messageID);
-    module.exports.run({ api, event, args: [] });
-  }
-}, 3600000); // 1 hour interval
+    const { messageID, threadID } = event;
+    const fs = require("fs");
+    const axios = require("axios");
+    const request = require("request");
+    const prompt = args.join(" ");
 
-// command to turn on/off auto execute
-module.exports.handleEvent = async ({ api, event }) => {
-  if (event.body === 'shoticron on') {
-    autoExecute = true;
-    api.sendMessage('Auto execute turned on!', event.threadID, event.messageID);
-  } else if (event.body === 'shoticron off') {
-    autoExecute = false;
-    api.sendMessage('Auto execute turned off!', event.threadID, event.messageID);
+    if (!prompt[0]) {
+      api.sendMessage("Downloading...", threadID, messageID);
+    }
+
+    try {
+      const response = await axios.post(`https://shoti-srv1.onrender.com/api/v1/get`, {
+        apikey: `shoti-1ha4h3do8at9a7ponr`
+      });
+
+      let path = __dirname + `/cache/shoti.mp4`;
+      const file = fs.createWriteStream(path);
+      const rqs = request(encodeURI(response.data.data.url));
+      rqs.pipe(file);
+      file.on(`finish`, () => {
+        setTimeout(function() {
+          api.setMessageReaction("✅", event.messageID, (err) => {}, true);
+          return api.sendMessage({
+            body: `𝖴𝗌𝖾𝗋𝗇𝖺𝗆𝖾 : @${response.data.data.user.username}\n𝖭𝗂𝖼𝗄𝗇𝖺𝗆𝖾 : ${response.data.data.user.nickname}`,
+            attachment: fs.createReadStream(path)
+          }, threadID);
+        }, 5000);
+      });
+      file.on(`error`, (err) => {
+        api.sendMessage(`Error: ${err}`, threadID, messageID);
+      });
+    } catch (err) {
+      api.sendMessage(`Error: ${err}`, threadID, messageID);
+    };
+  },
+
+  start: async ({ api, event }) => {
+    setInterval(async () => {
+      if (this.autoExecute) {
+        api.sendMessage("AUTO SHOTI SENT 🥵", event.threadID, event.messageID);
+        this.run({ api, event, args: [] });
+      }
+    }, 3600000); // 1 hour interval
+  },
+
+  handleEvent: async ({ api, event }) => {
+    if (event.body === 'hoticron on') {
+      this.autoExecute = true;
+      api.sendMessage('Auto execute turned on!', event.threadID, event.messageID);
+    } else if (event.body === 'hoticron off') {
+      this.autoExecute = false;
+      api.sendMessage('Auto execute turned off!', event.threadID, event.messageID);
+    }
   }
 };
