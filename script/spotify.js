89ea145274fa6cec3bf6201211383a6afe3e68fd
Alex3925 <120993369+Alex3925@users.noreply.github.com>
2024-08-15 20:51:08 +0800

    Update spotify.js

--- script/spotify.js
+++ script/spotify.js
@@ -5,8 +5,8 @@ module.exports.config = {
   name: "spotify",
   version: "1.0.0",
   role: 0,
-  credits: "Alex", //convert by Alex too
-  description: "Search and play music from Spotify", //api by Deku
+  credits: "Jonell Magallanes",
+  description: "Search and play music from Spotify", //api by jonell Magallanes cc project
   hasPrefix: false,
   usages: "[song name]",
   cooldown: 10
@@ -19,31 +19,31 @@ module.exports.run = async function ({ api, event, args }) {
   if (!listensearch) return api.sendMessage("Please provide the name of the song you want to search.", event.threadID, event.messageID);
 
   try {
-    api.sendMessage("🎵 | Searching for your music on Spotify. Please wait...", event.threadID, event.messageID);
+    api.sendMessage(" | Searching for your music on Spotify. Please wait...", event.threadID, event.messageID);
 
     const response = await axios.get(apiUrl);
-    const { data } = response.data;
+    const { platform, status, data } = response.data;
 
-    if (data) {
-      const { title, url } = data[0]; // assuming the API returns an array of results
+    if (status && platform === "Spotify") {
+      const { title, audio } = data;
 
       const filePath = `${__dirname}/cache/${Date.now()}.mp3`;
       const writeStream = fs.createWriteStream(filePath);
 
-      const audioResponse = await axios.get(url, { responseType: 'stream' });
+      const audioResponse = await axios.get(audio, { responseType: 'stream' });
       audioResponse.data.pipe(writeStream);
 
       writeStream.on('finish', () => {
         api.sendMessage({
-          body: `🎧 Here's your music from Spotify enjoy listening\n\nTitle:${title}\n\n💿 Now Playing...`,
+          body: ` Here's your music from Spotify enjoy listening\n\nTitle:${title}\n\n Now Playing...`,
           attachment: fs.createReadStream(filePath)
         }, event.threadID);
       });
     } else {
-      api.sendMessage("❓ | Sorry, couldn't find the requested music on Spotify.", event.threadID);
+      api.sendMessage(" | Sorry, couldn't find the requested music on Spotify.", event.threadID);
     }
   } catch (error) {
     console.error(error);
-    api.sendMessage("🚧 | An error occurred while processing your request.", event.threadID);
+    api.sendMessage(" | An error occurred while processing your request.", event.threadID);
   }
 };
